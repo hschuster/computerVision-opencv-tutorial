7859e4a1518021726ef08dd3db80bc25701011d1
Schuster <hschuster.xo@gmail.com>
2019-12-13 15:08:02 +0100

    Verschönerungen...

--- read_video.py
+++ read_video.py
@@ -1,15 +1,11 @@
 # USAGE
 # python detect_faces_video.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
 
-# import the necessary packages
-from imutils.video import VideoStream
-import numpy as np
-import argparse
-import imutils
 import time
+
 import cv2
-
-
+# import the necessary packages
+import imutils
 
 # initialize the video stream and allow the cammera sensor to warmup
 print("[INFO] starting video...")
@@ -21,7 +17,6 @@ while True:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 400 pixels
     success, frame = vs.read()
-    ###print('Read a new frame: ', success)
     frame = imutils.resize(frame, width=1200)
     cv2.imshow("Frame", frame)
 
