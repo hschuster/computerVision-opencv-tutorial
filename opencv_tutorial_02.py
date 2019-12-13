5e4b4e8ff5169339a689d897fc15f09ebc7c18c5
Schuster <hschuster.xo@gmail.com>
2019-12-13 14:20:47 +0100

    Fotos verkleinert...

--- opencv_tutorial_02.py
+++ opencv_tutorial_02.py
@@ -15,27 +15,27 @@ args = vars(ap.parse_args())
 # load the input image (whose path was supplied via command line
 # argument) and display the image to our screen
 image = cv2.imread(args["image"])
-resized = imutils.resize(image, width=500)
+# input image starting at x=320,y=60 at ending at x=420,y=160
+roi = image[378:1497, 66:913]
+resized = imutils.resize(roi, width=500)
 gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (7, 7), 0)  # 7,7 durch ausprobieren gefunden...
 cv2.imshow("Blurred", blurred)
 edged = cv2.Canny(blurred, 90, 190)  # 90,190 durch ausprobieren gefunden
-cv2.imshow("Edged 30 150", edged)
-cv2.waitKey(0)
+cv2.imshow("Edged 90 190", edged)
 
 # threshold the image by setting all pixel values less than 225
 # to 255 (white; foreground) and all pixel values >= 225 to 255
 # (black; background), thereby segmenting the image
-thresh = cv2.threshold(gray, 250, 250, cv2.THRESH_BINARY_INV)[1]
-cv2.imshow("Thresh", thresh)
-cv2.waitKey(0)
+# thresh = cv2.threshold(gray, 250, 250, cv2.THRESH_BINARY_INV)[1]
+# cv2.imshow("Thresh", thresh)
+# cv2.waitKey(0)
 
 # find contours (i.e., outlines) of the foreground objects in the
 # thresholded image
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
-output = image.copy()
+output = resized.copy()
 
 # loop over the contours
 for c in cnts:
@@ -43,12 +43,11 @@ for c in cnts:
 	# outline, then display the output contours one at a time
 	cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
 	cv2.imshow("Contours", output)
-	cv2.waitKey(0)
+	###cv2.waitKey(0)
 
 # draw the total number of contours found in purple
 text = "I found {} objects!".format(len(cnts))
-cv2.putText(output, text, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7,
-	(240, 0, 159), 2)
+cv2.putText(output, text, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (240, 0, 159), 2)
 cv2.imshow("Contours", output)
 cv2.waitKey(0)
 
